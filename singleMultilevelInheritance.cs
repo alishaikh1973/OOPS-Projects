67b2b60064a17732045e2b67eb5e4f614da349d1
ALI SHAIKH 1973 <139847421+alishaikh1973@users.noreply.github.com>
2023-09-17 04:08:47 +0530

    Add files via upload

---
+++ singleMultilevelInheritance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSPROJECT_TQ.Inheritance
+{
+   public  class Mobile
+        {
+            protected string Memory ;
+        public Mobile()
+        {
+                
+        }
+        public void Price()
+            {
+                Console.WriteLine(5000);
+            }
+            public virtual void Show()
+            {
+                Console.WriteLine(Memory);
+            }
+        }
+      public  class Apple : Mobile
+        {
+            string CameraPixel = "3pxl";
+            public override void Show()
+            {
+                Console.WriteLine(Memory);
+            }
+        }
+              public  class IphonePro : Mobile
+                {
+                    public override void Show()
+                    {
+                        Console.WriteLine(Memory);
+                    }
+                }
+
+    
+}
+    
+
