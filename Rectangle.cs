ce7e880c3b8308a8e2042594d2a8f3ea2c31ff12
ALI SHAIKH 1973 <139847421+alishaikh1973@users.noreply.github.com>
2023-09-17 03:29:47 +0530

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSPROJECT_TQ.AbstractProgram
+{
+    abstract public class Shape
+    {
+        public Shape()
+        {
+
+        }
+        public abstract string Draw(string name);
+        public abstract void Area();
+        public string Display()
+        {
+            return "This is shape class";
+        }
+
+    }
+    public class Rectangle :Shape
+    {
+        private int len;
+        private int b;
+        private double area;
+
+        public Rectangle(int len, int b) 
+        {
+            this.len = len;
+            this.b = b;
+        }
+        public override void Area()
+        {
+            area = len*b;
+        }
+        public override string Draw(string name) 
+        {
+            return name;
+        }
+        public string Print()
+        {
+            return $"Area of rectangle is = {area}";
+        }
+    }
+
+}
+
